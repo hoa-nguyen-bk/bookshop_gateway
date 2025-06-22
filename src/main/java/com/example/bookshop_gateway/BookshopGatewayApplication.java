3ceaf8761c3794e71505148a122eb863d69a57b2
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-06-22 09:51:49 +0700

    first commit

---
+++ src/main/java/com/example/bookshop_gateway/BookshopGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookshop_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopGatewayApplication.class, args);
+	}
+
+}
