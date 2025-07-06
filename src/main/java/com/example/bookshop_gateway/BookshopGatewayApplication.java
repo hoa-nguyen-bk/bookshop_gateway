570874fed05ee7c3df51ee7cd94e0956540ad858
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-07-06 10:42:12 +0700

    commit ând púh

--- src/main/java/com/example/bookshop_gateway/BookshopGatewayApplication.java
+++ src/main/java/com/example/bookshop_gateway/BookshopGatewayApplication.java
@@ -2,9 +2,7 @@ package com.example.bookshop_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class BookshopGatewayApplication {
---
+++ src/main/java/com/example/bookshop_gateway/Controller/FallbackController.java
@@ -0,0 +1,11 @@
+package com.example.bookshop_gateway.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+    @RequestMapping("/fallback")
+    public String fallback() {
+        return "Service is currently unavailable. Please try again later.";
+    }
+}
